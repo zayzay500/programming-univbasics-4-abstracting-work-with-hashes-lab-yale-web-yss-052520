16ec5e939df357104e8ebdb1d9354b7e6bee2e6f
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 01:08:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  {key: value}
+  {:key => value}
 end
 
 def read_from_hash(hash, key)
