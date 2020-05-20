f1eb7e732ed82251c1b9a8d471094a58a9452a6d
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 01:13:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -12,5 +12,8 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   if hash[key]
-else
+    hash[key] += 1
+  else
+    hash[key] => 1
+  end
 end
